0195344da841fe86c721efc7eaea0e67d31ad26e
Edgar <herreraperezedgar98@gmail.com>
2024-06-14 12:18:17 -0600

    Implemente crud de localidades

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LocalidadesModule } from './localidades/localidades.module';
 
 @Module({
-  imports: [],
+  imports: [LocalidadesModule],
   controllers: [AppController],
   providers: [AppService],
 })
